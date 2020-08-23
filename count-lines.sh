de8be7971093584875019a193c8d8d9256fcc207
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-08-23 13:33:38 +1000

    Don't count `import` statements as its style affect the count

--- count-lines.sh
+++ count-lines.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 total_lines() {
-    find "$1" -type file -path '*/src/*' -exec wc -l {} + | tail -1 | awk '{print $1}'
+    find "$1" -type file -path '*/src/*' -exec bash -c "grep -v '^import ' {} | wc -l" ';' \
+        | awk '{sum += $1}; END {print sum}'
 }
 
 for example in example--* ; do
