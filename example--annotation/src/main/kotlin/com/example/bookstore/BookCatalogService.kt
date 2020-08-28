103f74d342b2d62aad6c4a0e363f0dd3c6671067
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-08-28 21:00:53 +1000

    Autowired is not necessary if a component has only one constructor

--- example--annotation/src/main/kotlin/com/example/bookstore/BookCatalogService.kt
+++ example--annotation/src/main/kotlin/com/example/bookstore/BookCatalogService.kt
@@ -1,10 +1,9 @@
 package com.example.bookstore
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class BookCatalogService @Autowired constructor(private val bookRepo: BookRepo, private val authorRepo: AuthorRepo) {
+class BookCatalogService(private val bookRepo: BookRepo, private val authorRepo: AuthorRepo) {
     fun listBooks(): List<Book> = bookRepo.list().mapNotNull {
         raw -> authorRepo.resolveName(raw.authorId)?.let { Book(raw, it) }
     }
--- example--annotation/src/main/kotlin/com/example/bookstore/Router.kt
+++ example--annotation/src/main/kotlin/com/example/bookstore/Router.kt
@@ -1,6 +1,5 @@
 package com.example.bookstore
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.RequestPredicates.GET
@@ -9,7 +8,7 @@ import org.springframework.web.reactive.function.server.RouterFunctions
 import org.springframework.web.reactive.function.server.ServerResponse
 
 @Component
-class Router @Autowired constructor(private val bookCatalog: BookCatalogService) {
+class Router(private val bookCatalog: BookCatalogService) {
     val route: RouterFunction<ServerResponse> = RouterFunctions.route(
         GET("/books"), {
             ServerResponse.ok().body(BodyInserters.fromObject(
